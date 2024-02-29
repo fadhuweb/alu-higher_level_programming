37ce4bf491a352fa20a6014db64b765edf23e1cc
fadhuweb <f.abdulazee@alustudent.com>
2024-02-28 22:42:10 -0800

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,9 @@
 
 for i in range(9):
     for j in range(i + 1, 10):
-        if (i == 8 and j == 9):
-            print("{num:02}".format(num=((i * 10) + j)), end="\n")
+        if i == 8 and j == 9:
+            print("{num:02}".format(num=((i * 10) + j)))
         else:
             print("{num:02}".format(num=((i * 10) + j)), end=", ")
+)
 
