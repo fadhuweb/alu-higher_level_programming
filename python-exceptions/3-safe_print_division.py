15d4a08a2d4cba88d66f8653dbc224d66c46d3bd
fadhuweb <f.abdulazee@alustudent.com>
2024-03-01 12:57:44 -0800

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 
 
 def safe_print_division(a, b):
-    """Returns the division of a by b."""
+    #Returns the division of a by b.
     try:
         div = a / b
     except (TypeError, ZeroDivisionError):
