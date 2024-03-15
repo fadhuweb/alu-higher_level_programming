198067f10d02dcb4300bb55f16118c2ca512e38c
Fadhuweb <f.abdulazee@alustudent.com>
2024-03-15 03:26:28 -0700

    task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """
 This is the "Rectangle"  module.
-This module provides a simple Rectangle class.
+This module provides a Rectangle class.
 """
 
 
 class Rectangle:
-    """A Rectangle class with attributes width and height, and
-    methods area and perimiter.
+    """A Rectangle class with attributes width and height,
+    methods area, perimeter, print, str, repr, and del, and
+    class attribute number_of_instances that keeps track of # of instances,
+    and class attribute print_symbol which is used as symbol for printing.
     """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -36,6 +43,25 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __str__(self):
+        total = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                try:
+                    total += str(self.print_symbol)
+                except Exception:
+                    total += type(self).print_symbol
+            if i is not self.__height - 1:
+                total += "\n"
+        return total
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     def area(self):
         return self.__width * self.__height
